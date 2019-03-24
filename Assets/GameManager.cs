f77a67127882d99c75c091106a1faf835e7d3892
Raikish <albert.montagut.casero@gmail.com>
2019-03-24 17:01:15 +0100

    refactored and added colors

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,25 +4,54 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Vector2 gridSize;
+    public int numTables = 3;
+    public int maxMultiplyBy = 10;
     public GameObject toInstantiate;
 
     void Start()
     {
-        for (int i = 0; i < gridSize.y; i++)
+        for (int table = 0; table < numTables; table++)
         {
-            for (int j = 0; j < gridSize.x; j++)
+            for (int multiplyBy = 0; multiplyBy < maxMultiplyBy; multiplyBy++)
             {
-                for (int z = 0; z < (i + 1) * (j+1); z++) //resultat taula multiplicar ex: 4x4 = 16 figures
+                int multiplicationResult = (table + 1) * (multiplyBy + 1);
+                float widthToInstantiate = toInstantiate.GetComponent<Renderer>().bounds.size.x;
+                float offset = widthToInstantiate;
+
+                if (multiplyBy == 0) // first 
                 {
-                    float widthToInstantiate = toInstantiate.GetComponent<Renderer>().bounds.size.x;
-                    float offset = widthToInstantiate;
+                    for (int z = 0; z < multiplicationResult; z++)
+                    {
                     Vector3 nextPosition = new Vector3(
-                        (float)j * (offset + widthToInstantiate + widthToInstantiate / 2) ,
+                        (float)multiplyBy * (widthToInstantiate + widthToInstantiate / 2),
                         (float)z * widthToInstantiate + widthToInstantiate / 2,
-                        (float)i * (offset + widthToInstantiate + widthToInstantiate / 2));
-                    GameObject instantiate = Instantiate(toInstantiate, nextPosition, new Quaternion(0, 0, 0, 0));
-                    
+                        (float)table * (widthToInstantiate + widthToInstantiate / 2));
+
+                        GameObject instantiated = Instantiate(toInstantiate, nextPosition, new Quaternion(0, 0, 0, 0));
+                        Renderer instantiadedRender = instantiated.GetComponent<Renderer>();
+                        instantiadedRender.material.color = new Color32(
+                            (byte)((z + 1) * 255 / multiplicationResult),
+                            0,
+                            0,
+                            255);
+                    }
+                }
+
+                for (int i = 0; i < multiplicationResult; i++) //resultat taula multiplicar ex: 4x4 = 16 figures
+                {
+
+                    Vector3 nextPosition = new Vector3(
+                        (float)multiplyBy * (offset + widthToInstantiate + widthToInstantiate / 2) ,
+                        (float)i * widthToInstantiate + widthToInstantiate / 2,
+                        (float)table * (offset + widthToInstantiate + widthToInstantiate / 2));
+
+                    GameObject instantiated = Instantiate(toInstantiate, nextPosition, new Quaternion(0, 0, 0, 0));
+                    Renderer instantiadedRender = instantiated.GetComponent<Renderer>();
+                    instantiadedRender.material.color = new Color32(
+                        0,
+                        (byte)(multiplyBy * 255 / maxMultiplyBy),
+                        (byte)((i + 1) * 255 / multiplicationResult),
+                        255);
                 }
             }
         }
