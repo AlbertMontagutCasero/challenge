43cfe30316d5deb6e676e6f0b8df17292c392191
Raikish <albert.montagut.casero@gmail.com>
2019-03-25 17:36:05 +0100

    Cleaning up

--- Assets/WaveEffect/WaveEffectManager.cs
+++ Assets/WaveEffect/WaveEffectManager.cs
@@ -6,9 +6,7 @@ public class WaveEffectManager : MonoBehaviour
 {
     public GameObject toInstantiate;
     public Transform startPoint;
-    public float frequency;
     private float widthToInstantiate;
-    private float timeElapsed = 0;
     private List<Transform> instancedShapes = new List<Transform>();
     private int count = 0;
     private int max = 360;
@@ -18,50 +16,6 @@ public class WaveEffectManager : MonoBehaviour
         widthToInstantiate = toInstantiate.GetComponent<Renderer>().bounds.size.x;
         StartCoroutine(SpawnShapes());
 
-    }
-
-    void Update()
-    {
-        //timeElapsed += Time.deltaTime;
-        //if (timeElapsed >= frequency)
-        //{
-        //    timeElapsed = 0;
-        //    GameObject instantiated = Instantiate(toInstantiate, startPoint.position, new Quaternion(0, 0, 0, 0));
-        //    instancedShapes.Add(instantiated.transform);
-        //    foreach (Transform transformInstancedShape in instancedShapes)
-        //    {
-        //        transformInstancedShape.position = new Vector3(
-        //            transformInstancedShape.transform.position.x - widthToInstantiate,
-        //            transformInstancedShape.position.y,
-        //            transformInstancedShape.position.z);
-        //    }
-        //    Debug.Log(Time.time);
-        //    instantiated.transform.localScale = new Vector3(1, Mathf.Sin(Time.time * frequency) * 10, 1);
-        //}
-
-        //GameObject instantiated = Instantiate(toInstantiate, startPoint.position, new Quaternion(0, 0, 0, 0));
-        //instancedShapes.Add(instantiated.transform);
-        //foreach (Transform transformInstancedShape in instancedShapes)
-        //{
-        //    transformInstancedShape.position = new Vector3(
-        //        transformInstancedShape.transform.position.x - widthToInstantiate,
-        //        transformInstancedShape.position.y,
-        //        transformInstancedShape.position.z);
-        //}
-        //Debug.Log(Time.time);
-        //instantiated.transform.localScale = new Vector3(1, Mathf.Sin(Time.time) * 20, 1);
-
-        //count = (count +1 ) % max;
-        //GameObject instantiated = Instantiate(toInstantiate, startPoint.position, new Quaternion(0, 0, 0, 0));
-        //instancedShapes.Add(instantiated.transform);
-        //foreach (Transform transformInstancedShape in instancedShapes)
-        //{
-        //    transformInstancedShape.position = new Vector3(
-        //        transformInstancedShape.transform.position.x - widthToInstantiate,
-        //        transformInstancedShape.position.y,
-        //        transformInstancedShape.position.z);
-        //}
-        //instantiated.transform.localScale = new Vector3(1, Mathf.Sin(count * max) * 20, 1);
     }
 
     IEnumerator SpawnShapes()
